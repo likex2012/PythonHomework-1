2b0fe1d39395cd4fe7106e20c24654d70a79c224
Sergey <likex2012@yandex.ru>
2022-09-04 19:29:40 +0300

    Homework by lesson 2

---
+++ Homework2.py
@@ -0,0 +1,53 @@
+#Work 1
+# li = [1, 3, 'good day', {'winter': 'february', 'summer': 'july', 'spring': 'april'}, [1, 3, 5, 7], True]
+# print(type(li))
+# print(type(li[0]))
+# print(type(li[1]))
+# print(type(li[2]))
+# print(type(li[3]))
+# print(type(li[4]))
+# print(type(li[5]))
+
+# #Work 2
+# l = input("Введите элементы списка: ").split()
+# l[:-1:2], l[1::2] = l[1::2], l[:-1:2]
+# print(l)
+
+#Work 3- через list
+# t = int(input('Введите месяц от 1 до 12: '))
+# l1 = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+# if t in l1[:3]:
+#     print('Зима')
+# elif t in l1[3:6]:
+#     print('Весна')
+# elif t in l1[6:9]:
+#     print('Лето')
+# else: print('Осень')
+
+#Work 3- через dict
+# t = int(input('Введите месяц от 1 до 12: '))
+# d = {'Зима': [12, 1, 2], 'Весна': [3, 4, 5], 'Лето': [6, 7, 8], 'Осень': [9, 10, 11]}
+# for month in d:
+#     if t in d[month]:
+#         print(month)
+#         break
+
+#Work 4
+# sar = input('Введите данные: ').lower().split()
+# for i, el in enumerate(sar, 1):
+#     if len(el) <= 10:
+#         print(f' {i}. {el}')
+
+#Work 5
+# rating = [7, 5, 3, 3, 2]
+# el = int(input('Enter the number: '))
+# for a in rating:
+#     if el > int(rating[0]):
+#         rating.insert(0, el)
+#         print(rating)
+#         break
+#     else:
+#         rating.append(el)
+#         rating.sort(reverse=True)
+#         print(rating)
+#         break
